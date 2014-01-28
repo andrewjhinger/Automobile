9851feb575ff97895d9640511422ccf4c022ace3
ahinger <andrewjhinger@gmail.com>
2014-01-28 10:14:31 -0500

    first

---
+++ Automobile/Automobile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Automobile
+{
+    public partial class Automobile : Form
+    {
+        private MyAutomobile _myAutomobile;
+
+        public Automobile()
+        {
+            _myAutomobile = new MyAutomobile();
+            _myAutomobile.Topspeed = 100;
+            _myAutomobile.Mpg = 45;
+            _myAutomobile.Tanksize = 15;
+            _myAutomobile.Estdist = _myAutomobile.Mpg * _myAutomobile.Estdist;
+            _myAutomobile.Numdoors = 2;
+            _myAutomobile.Sunroof = true;
+            _myAutomobile.Color = "Blue";
+            InitializeComponent();
+            Console.WriteLine("Top speed: {0}\nMpg: {1}\nTanksize: {2}\nEstdist: {3}\nNumdoors: {4}\nSunroof: {5}\nColor: {6}", _myAutomobile.Topspeed, _myAutomobile.Mpg, _myAutomobile.Tanksize, _myAutomobile.Estdist, _myAutomobile.Numdoors, _myAutomobile.Sunroof, _myAutomobile.Color);
+
+        }
+    }
+}
---
+++ Automobile/MyAutomobile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Automobile
+{
+    public class MyAutomobile
+    {
+        private int _topspeed;
+
+        public int Topspeed
+        {
+            get { return _topspeed; }
+            set { _topspeed = value; }
+        }
+        private double _mpg;
+
+        public double Mpg
+        {
+            get { return _mpg; }
+            set { _mpg = value; }
+        }
+        private double _tanksize;
+
+        public double Tanksize
+        {
+            get { return _tanksize; }
+            set { _tanksize = value; }
+        }
+        private double _estdist;
+
+        public double Estdist
+        {
+            get { return _estdist; }
+            set { _estdist = value; }
+        }
+        private int _numdoors;
+
+        public int Numdoors
+        {
+            get { return _numdoors; }
+            set { _numdoors = value; }
+        }
+        private bool _sunroof;
+
+        public bool Sunroof
+        {
+            get { return _sunroof; }
+            set { _sunroof = value; }
+        }
+        private string _color;
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+    }
+}
